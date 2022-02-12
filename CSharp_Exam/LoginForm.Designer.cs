1cb413611a02d22fa193228864614f8c3d3bed95
valik <valik324453@mail.ru>
2022-02-12 15:38:42 +0200

    Create design for LoginForm. Make a basic functional for LoginForm: clear fields, exit, focus, login test

---
+++ CSharp_Exam/LoginForm.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace CSharp_Exam
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button btnRegistration;
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pPassword = new System.Windows.Forms.PictureBox();
+            this.pUserName = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.lClearFields = new System.Windows.Forms.Label();
+            this.lExit = new System.Windows.Forms.Label();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            btnRegistration = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pPassword)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pUserName)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRegistration
+            // 
+            btnRegistration.BackColor = System.Drawing.Color.White;
+            btnRegistration.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnRegistration.Font = new System.Drawing.Font("Bahnschrift", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            btnRegistration.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(155)))), ((int)(((byte)(2)))));
+            btnRegistration.Location = new System.Drawing.Point(26, 365);
+            btnRegistration.Name = "btnRegistration";
+            btnRegistration.Size = new System.Drawing.Size(236, 33);
+            btnRegistration.TabIndex = 7;
+            btnRegistration.Text = "Registration";
+            btnRegistration.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Location = new System.Drawing.Point(54, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 68);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LOG IN";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(158)))), ((int)(((byte)(0)))));
+            this.panel1.Location = new System.Drawing.Point(26, 214);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(236, 1);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(158)))), ((int)(((byte)(0)))));
+            this.panel2.Location = new System.Drawing.Point(26, 274);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(236, 1);
+            this.panel2.TabIndex = 3;
+            // 
+            // pPassword
+            // 
+            this.pPassword.Image = global::CSharp_Exam.Properties.Resources._lock;
+            this.pPassword.Location = new System.Drawing.Point(35, 243);
+            this.pPassword.Name = "pPassword";
+            this.pPassword.Size = new System.Drawing.Size(25, 25);
+            this.pPassword.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pPassword.TabIndex = 2;
+            this.pPassword.TabStop = false;
+            this.pPassword.Click += new System.EventHandler(this.pPassword_Click);
+            // 
+            // pUserName
+            // 
+            this.pUserName.Image = global::CSharp_Exam.Properties.Resources.user;
+            this.pUserName.Location = new System.Drawing.Point(35, 183);
+            this.pUserName.Name = "pUserName";
+            this.pUserName.Size = new System.Drawing.Size(25, 25);
+            this.pUserName.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pUserName.TabIndex = 2;
+            this.pUserName.TabStop = false;
+            this.pUserName.Click += new System.EventHandler(this.pUserName_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CSharp_Exam.Properties.Resources.Keys_21;
+            this.pictureBox1.Location = new System.Drawing.Point(91, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 102);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(158)))), ((int)(((byte)(0)))));
+            this.btnLogIn.FlatAppearance.BorderSize = 0;
+            this.btnLogIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogIn.Font = new System.Drawing.Font("Bahnschrift", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLogIn.ForeColor = System.Drawing.Color.White;
+            this.btnLogIn.Location = new System.Drawing.Point(26, 325);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(236, 33);
+            this.btnLogIn.TabIndex = 4;
+            this.btnLogIn.Text = "LOG IN";
+            this.btnLogIn.UseVisualStyleBackColor = false;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // lClearFields
+            // 
+            this.lClearFields.AutoSize = true;
+            this.lClearFields.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lClearFields.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(212)))), ((int)(((byte)(227)))));
+            this.lClearFields.Location = new System.Drawing.Point(153, 304);
+            this.lClearFields.Name = "lClearFields";
+            this.lClearFields.Size = new System.Drawing.Size(98, 18);
+            this.lClearFields.TabIndex = 5;
+            this.lClearFields.Text = "Clear Fields";
+            this.lClearFields.Click += new System.EventHandler(this.lClearFields_Click);
+            // 
+            // lExit
+            // 
+            this.lExit.AutoSize = true;
+            this.lExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(212)))), ((int)(((byte)(227)))));
+            this.lExit.Location = new System.Drawing.Point(124, 410);
+            this.lExit.Name = "lExit";
+            this.lExit.Size = new System.Drawing.Size(41, 20);
+            this.lExit.TabIndex = 5;
+            this.lExit.Text = "Exit";
+            this.lExit.Click += new System.EventHandler(this.lExit_Click);
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbUserName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.tbUserName.Location = new System.Drawing.Point(66, 183);
+            this.tbUserName.Multiline = true;
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(194, 24);
+            this.tbUserName.TabIndex = 6;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.tbPassword.Location = new System.Drawing.Point(66, 243);
+            this.tbPassword.Multiline = true;
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(194, 24);
+            this.tbPassword.TabIndex = 6;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(290, 439);
+            this.Controls.Add(btnRegistration);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUserName);
+            this.Controls.Add(this.lExit);
+            this.Controls.Add(this.lClearFields);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pPassword);
+            this.Controls.Add(this.pUserName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(212)))), ((int)(((byte)(222)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pPassword)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pUserName)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pUserName;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pPassword;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.Label lClearFields;
+        private System.Windows.Forms.Label lExit;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.TextBox tbPassword;
+    }
+}
+
---
+++ CSharp_Exam/LoginForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_Exam
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            if(tbUserName.Text == "Valik" && tbPassword.Text == "1234")
+            {
+                new Form2().Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Wrong username or password");
+                tbUserName.Clear();
+                tbPassword.Clear();
+                tbUserName.Focus();
+            }
+        }
+
+        private void lClearFields_Click(object sender, EventArgs e)
+        {
+            tbUserName.Clear();
+            tbPassword.Clear();
+            tbUserName.Focus();
+        }
+
+        private void lExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pUserName_Click(object sender, EventArgs e)
+        {
+            tbUserName.Focus();
+        }
+
+        private void pPassword_Click(object sender, EventArgs e)
+        {
+            tbPassword.Focus();
+        }
+    }
+}
